b6632ec42faf4db87cbec361110a64b54ddaa018
Fabian Lins <local@local.local>
2023-03-14 01:18:03 +0200

    list of skills

--- front-page.php
+++ front-page.php
@@ -1,59 +1,57 @@
 <?php
 get_header();
 ?>
-<input type="checkbox" name="list-of-skills" id="list-of-skills">
-<div class="about-me-container">
-    <section class="about-me">
-        <h2 class="h2 mt-header h-pl animation-slide-in-right">
-            <?php the_title() ?>
-        </h2>
-        <div class="container">
-            <div class="grid">
-                <div class="left animation-slide-in-right">
-                    <div class="text-box">
-                        <?php
-                        if (have_posts()) {
-                            while (have_posts()) {
-                                the_post();
-                                the_content();
-                            }
+<section class="about-me">
+    <h2 class="h2 mt-header h-pl animation-slide-in-right">
+        <?php the_title() ?>
+    </h2>
+    <div class="container">
+        <div class="grid">
+            <div class="left animation-slide-in-right">
+                <div class="text-box">
+                    <?php
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post();
+                            the_content();
                         }
-                        ?>
-                    </div>
-                    <div class="photo">
-                        <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
-                    </div>
-                    <div class="buttons">
-                        <a class="btn-1" href="#anchor">
-                            <label for="list-of-skills" class="prim-btn big animation-button-blink">
-                                <?php echo get_the_title('22'); ?>
-                            </label>
+                    }
+                    ?>
+                </div>
+                <div class="photo">
+                    <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
+                </div>
+                <div class="buttons">
+                    <div class="btn-1">
+                        <a href="<?php echo (get_permalink('22')); ?>"
+                            class="prim-btn big animation-button-blink animation-delay">
+                            <?php echo get_the_title('22'); ?>
                         </a>
-                        <div class="btn-2">
-                            <a href="<?php echo (get_permalink('18')); ?>"
-                                class="prim-btn big animation-button-blink animation-delay">
-                                <?php echo get_the_title('18'); ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="right">
-                    <div class="photo animation-slide-in-left animation-spin-in">
-                        <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
-                    </div>
-                </div>
 
+                    </div>
+                    <div class="btn-2">
+                        <a href="<?php echo (get_permalink('18')); ?>"
+                            class="prim-btn big animation-button-blink animation-delay">
+                            <?php echo get_the_title('18'); ?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="photo animation-slide-in-left animation-spin-in">
+                    <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
+                </div>
             </div>
 
-
         </div>
-        <img class="wheel-icon animation-spin" src="<?php echo (wp_get_attachment_image_src(35)[0]); ?>" alt="" />
-    </section>
-    <div id="anchor"></div>
-    <section class="list-of-skills">
-        lol
-    </section>
-</div>
+    </div>
+    <img class="wheel-icon animation-spin" src="<?php echo (wp_get_attachment_image_src(35)[0]); ?>" alt="" />
+</section>
+<div id="anchor"></div>
+<section class="list-of-skills">
+    lol
+</section>
+
 
 <?php
 get_footer();
--- page.php
+++ page.php
@@ -1,49 +1,70 @@
 <?php
 get_header();
 ?>
-<section class="about-me">
+<section class="about-me list-of-skills">
     <h2 class="h2 mt-header h-pl animation-slide-in-right">
         <?php the_title() ?>
     </h2>
     <div class="container">
-        <div class="grid">
-            <div class="left animation-slide-in-right">
-                <div class="text-box">
-                    <?php
-                    if (have_posts()) {
-                        while (have_posts()) {
-                            the_post();
-                            the_content();
-                        }
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                $content       = get_the_content();
+                $categories    = explode("<!-- /wp:list -->", $content, -1);
+                $categoriesLen = count($categories);
+                for ($i = 0; $i < $categoriesLen; $i++) {
+                    $categories[$i] = $categories[$i] . "<!-- /wp:list -->";
+                }
+                for ($i = 0; $i < $categoriesLen; $i++) {
+                    if (preg_match("/<h(1|2|3|4|5|6).*?>(.*?)<\/h.*?>/", $categories[$i], $matches)) {
+                        //print_r($matches);
+                        $listStart = strpos($categories[$i], "<ul>");
+                        $listContent = substr($categories[$i],$listStart);
+                        $listContent = str_replace("<ul>","",$listContent);
+                        $listContent = str_replace("</ul>","",$listContent);
+                        $listContent = str_replace("<!-- /wp:list -->","",$listContent);
+                        $listContent = str_replace("<!-- wp:list-item -->","",$listContent);
+                        $listContent = explode("<!-- /wp:list-item -->", $listContent, -1);
+                        $listContentLen = count($listContent);
+                        echo ("<div class='cateogory-container grid'>");
+                            echo ("<div class='left'>");
+                                echo ("<h2 class='skill-heading'>" . $matches[2] . "</h2>");
+                                echo ("</h2>");// skill-heading
+                                echo ("<ul class='skills'>");
+                                foreach($listContent as $currListContent){
+                                    $stars = 0;
+                                    if (preg_match("/{(1|2|3|4|5)s}/", $currListContent, $matches)) {
+                                        $stars = $matches[1];
+                                        $currListContent = str_replace($matches[0],"",$currListContent);
+                                    }
+                                    echo ("<div class='skills-point grid'>");
+                                        echo ($currListContent);
+                                        echo ("<div class='rating'>");
+                                        for ($j = 0; $j < $stars; $j++) {
+                                            $src = wp_get_attachment_image_src(57)[0];
+                                            echo("<img class='star-full' src=".$src." alt='' /> ");
+                                        }
+                                        for ($j = 0; $j < 5 - $stars; $j++) {
+                                            $src = wp_get_attachment_image_src(56)[0];
+                                            echo("<img class='star-outline' src=".$src." alt='' /> ");
+                                        }
+                                        echo ("</div>");//rating
+
+                                    echo ("</div>"); // skills-point
+                                }
+                                echo ("</ul>");// skills
+                            echo ("</div>");// left
+                            echo ("<div class='right'>");
+                                echo ("container-right-content");
+                            echo ("</div>");// right
+                        echo ("</div>");// category-container
                     }
-                    ?>
-                </div>
-                <div class="photo">
-                    <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
-                </div>
-                <div class="buttons">
-                    <div class="btn-1">
-                        <a href="<?php echo (get_permalink('22')); ?>" class="prim-btn big animation-button-blink">
-                            <?php echo get_the_title('22'); ?>
-                        </a>
-                    </div>
-                    <div class="btn-2">
-                        <a href="<?php echo (get_permalink('18')); ?>"
-                            class="prim-btn big animation-button-blink animation-delay">
-                            <?php echo get_the_title('18'); ?>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="right">
-                <div class="photo animation-slide-in-left animation-spin-in">
-                    <img src="/wp-content/themes/custom-theme/img/lucico.jpg" alt="">
-                </div>
-            </div>
-
-        </div>
-
-
+                }
+            }
+        }
+        //print_r($categories);
+        //for($i = 0; $i < $lists; $i++){ //} //print_r($lists); //print_r($content); } } ?>
     </div>
     <img class="wheel-icon animation-spin" src="<?php echo (wp_get_attachment_image_src(35)[0]); ?>" alt="" />
 </section>
@@ -51,5 +72,3 @@ get_header();
 get_footer();
 ?>
 </body>
-
-</html>
