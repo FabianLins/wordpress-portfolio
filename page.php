d45a76ba33083c1930c3b8f958a6e8b9daf01308
Fabian Lins <local@local.local>
2023-03-14 22:44:21 +0200

    list of skills - front end

--- page.php
+++ page.php
@@ -18,7 +18,7 @@ get_header();
                 }
                 for ($i = 0; $i < $categoriesLen; $i++) {
                     if (preg_match("/<h(1|2|3|4|5|6).*?>(.*?)<\/h.*?>/", $categories[$i], $matches)) {
-                        //print_r($matches);
+                        //print_r($matches);x
                         $listStart = strpos($categories[$i], "<ul>");
                         $listContent = substr($categories[$i],$listStart);
                         $listContent = str_replace("<ul>","",$listContent);
@@ -29,8 +29,9 @@ get_header();
                         $listContentLen = count($listContent);
                         echo ("<div class='cateogory-container grid'>");
                             echo ("<div class='left'>");
-                                echo ("<h2 class='skill-heading'>" . $matches[2] . "</h2>");
-                                echo ("</h2>");// skill-heading
+                                echo ("<h2 class='skill-heading'>");
+                                    echo ($matches[2]);
+                                echo ("</h2>"); // skill-heading
                                 echo ("<ul class='skills'>");
                                 foreach($listContent as $currListContent){
                                     $stars = 0;
@@ -42,22 +43,52 @@ get_header();
                                         echo ($currListContent);
                                         echo ("<div class='rating'>");
                                         for ($j = 0; $j < $stars; $j++) {
-                                            $src = wp_get_attachment_image_src(57)[0];
-                                            echo("<img class='star-full' src=".$src." alt='' /> ");
+                                            $src = wp_get_attachment_image_src(71)[0];
+                                            echo ("<img class='star-full' src=" . $src . " alt='' /> ");
                                         }
                                         for ($j = 0; $j < 5 - $stars; $j++) {
-                                            $src = wp_get_attachment_image_src(56)[0];
-                                            echo("<img class='star-outline' src=".$src." alt='' /> ");
+                                            $src = wp_get_attachment_image_src(67)[0];
+                                            echo ("<img class='star-outline' src=" . $src . " alt='' /> ");
                                         }
-                                        echo ("</div>");//rating
+                                        echo ("</div>"); // rating
 
                                     echo ("</div>"); // skills-point
                                 }
                                 echo ("</ul>");// skills
                             echo ("</div>");// left
+                            if($i === 0){
+                                echo ("<div class='right'>");
+                                echo ("<input id='red-dot' class='mac-dot-input' type='radio' name='mac-dots'>");
+                                echo ("<input id='yellow-dot' class='mac-dot-input' type='radio' name='mac-dots'>");
+                                echo ("<input id='green-dot' class='mac-dot-input' type='radio' name='mac-dots' checked>");
+                                    echo ("<div class='icon front-end-icon'>");
+                                        echo ("<div class='window-container-container'>");
+                                            echo ("<div class='window-container'>");
+                                                echo ("<div class='window'>");
+                                                    echo ("<div class='top'>");
+                                                        echo ("<div class='dots'>");
+                                                            echo ("<label for='red-dot' class='red animation-mac-red-blink'></label>"); // red
+                                                            echo ("<label for='yellow-dot' class='yellow animation-mac-yellow-blink'></label>"); // yellow
+                                                            echo ("<label for='green-dot'class='green animation-mac-green-blink'></label>"); // green
+                                                        echo ("</div>"); // dots
+                                                    echo ("</div>"); // top
+                                                    echo ("<div class='bottom'>");
+                                                        echo ("<div class='tag-close'>");
+                                                        echo ("</div>"); // tag-close
+                                                    echo ("</div>"); // bottom
+                                                echo ("</div>"); // window
+                                            echo ("</div>"); // window-container
+                                        echo ("</div>"); // window-container-container
+                                    echo ("</div>"); // front-end-icon
+                                echo ("</div>"); // right
+                            }
+                            elseif($i === 1){
                             echo ("<div class='right'>");
-                                echo ("container-right-content");
-                            echo ("</div>");// right
+                                echo ("<div class='back-end-icon'>");
+                                echo ("</div>"); // back-end-icon
+                            echo ("</div>"); // right
+
+                            }
                         echo ("</div>");// category-container
                     }
                 }
